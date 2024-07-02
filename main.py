33e33b6a14d1b3c7ddf6e5e93869dafdcb07bfc6
ikeda042 <ikeda042main@gmail.com>
2024-07-02 16:27:07 +0900

    main

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from qiime2 import Artifact
 import pandas as pd
 
 aft = Artifact.load("rep-seqs.qza")
-table_df = aft.view(pd.DataFrame)
 
-print(table_df)
+sequences = aft.view(qiime2.Metadata)
+
+sequences_df = sequences.to_dataframe()
+
+print(sequences_df)
+
+sequences_df.to_csv("sequences.csv", index=False)
