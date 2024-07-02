e0edda422c1c4bf846b30550c7ca83f5bad2ad0b
ikeda042 <ikeda042main@gmail.com>
2024-07-02 20:55:27 +0900

    router

--- router.py
+++ router.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from models import Sequence
-from main import load_qza
 import uvicorn
 import aiofiles
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+import qiime2
+import pandas as pd
+from qiime2 import Artifact
 
 app = FastAPI(
     docs_url="/api/docs", redoc_url="/api/redoc", openapi_url="/api/openapi.json"
@@ -18,6 +22,29 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+executor = ThreadPoolExecutor()
+
+
+def load_qza_sync(file_path: str) -> list[Sequence]:
+    aft: Artifact = Artifact.load(file_path)
+    sequences: qiime2.Metadata = aft.view(qiime2.Metadata)
+    sequences_df: pd.DataFrame = sequences.to_dataframe()
+    ret: dict[str, str] = {
+        i: sequences_df.loc[i, "Sequence"] for i in sequences_df.index
+    }
+    fasta_content = "\n".join(f">{i}\n{ret[i]}" for i in ret)
+    return ret, fasta_content
+
+
+async def load_qza(file_path: str) -> list[Sequence]:
+    loop = asyncio.get_event_loop()
+    ret, fasta_content = await loop.run_in_executor(executor, load_qza_sync, file_path)
+
+    async with aiofiles.open(f"{file_path.split('.')[0]}.fasta", "w") as f:
+        await f.write(fasta_content)
+
+    return [Sequence(id=i, sequence=ret[i]) for i in ret]
 
 
 @app.get("/")
