69e420e3bd63dcded2117b956fcf8016cbd72067
ikeda042 <ikeda042main@gmail.com>
2024-07-02 16:43:53 +0900

    Fasta

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ async def load_qza(file_path: str) -> list[Sequence]:
     sequences = aft.view(qiime2.Metadata)
     sequences_df = sequences.to_dataframe()
     ret = {i: sequences_df.loc[i, "Sequence"] for i in sequences_df.index}
+    async with aiofiles.open(f"{file_path.split('.')[0]}.fasta", "w") as f:
+        for i in ret:
+            await f.write(f">{i}\n{ret[i]}\n")
     return [Sequence(id=i, sequence=ret[i]) for i in ret]
 
 
