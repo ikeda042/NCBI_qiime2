56cb5b501b27c35e4186fe12bebb95c4d026e166
ikeda042 <ikeda042main@gmail.com>
2024-07-02 16:46:15 +0900

    api

--- router.py
+++ router.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from models import Sequence
+from main import load_qza
 
 
 app = FastAPI()
@@ -15,6 +17,6 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/qza/{file_path}", response_model=list[Sequence])
+async def get_qza(file_path: str) -> list[Sequence]:
+    return await load_qza(file_path)
