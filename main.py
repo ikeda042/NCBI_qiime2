9a0749276120d229d87a211b93ecd3d45aa6533c
ikeda042 <ikeda042main@gmail.com>
2024-07-02 17:10:35 +0900

    annotations

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from qiime2 import Artifact
 import aiofiles
 import asyncio
 from models import Sequence
+import pandas as pd
 
 
 async def load_qza(file_path: str) -> list[Sequence]:
     aft: Artifact = Artifact.load(file_path)
-    sequences = aft.view(qiime2.Metadata)
-    sequences_df = sequences.to_dataframe()
-    ret = {i: sequences_df.loc[i, "Sequence"] for i in sequences_df.index}
+    sequences: qiime2.Metadata = aft.view(qiime2.Metadata)
+    sequences_df: pd.DataFrame = sequences.to_dataframe()
+    ret: dict[str, str] = {
+        i: sequences_df.loc[i, "Sequence"] for i in sequences_df.index
+    }
     async with aiofiles.open(f"{file_path.split('.')[0]}.fasta", "w") as f:
         for i in ret:
             await f.write(f">{i}\n{ret[i]}\n")
@@ -17,6 +20,6 @@ async def load_qza(file_path: str) -> list[Sequence]:
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(load_qza("rep-seqs.qza"))
+    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+    res: list[Sequence] = loop.run_until_complete(load_qza("rep-seqs.qza"))
     print(res)
