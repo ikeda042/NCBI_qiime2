8a79121b0dcf1539646a81a63cef87d7d361b7c7
ikeda042 <ikeda042main@gmail.com>
2024-07-02 20:39:05 +0900

    DCTG

--- router.py
+++ router.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import Sequence
 from main import load_qza
+import uvicorn
 
-
-app = FastAPI()
+app = FastAPI(
+    docs_url="/api/docs", redoc_url="/api/redoc", openapi_url="/api/openapi.json"
+)
 
 origins = ["*"]
 
@@ -20,3 +22,7 @@ app.add_middleware(
 @app.get("/qza/{file_path}", response_model=list[Sequence])
 async def get_qza(file_path: str) -> list[Sequence]:
     return await load_qza(file_path)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
