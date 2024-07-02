8d783d2c38eb7d2f855346f8effdfb606992ea41
ikeda042 <ikeda042main@gmail.com>
2024-07-02 16:42:15 +0900

    aio

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import qiime2
 from qiime2 import Artifact
 import aiofiles
 import asyncio
-from pydantic import BaseModel
-
-aft = Artifact.load("rep-seqs.qza")
-
-sequences = aft.view(qiime2.Metadata)
-
-sequences_df = sequences.to_dataframe()
-
-ret = {i: sequences_df.loc[i, "Sequence"] for i in sequences_df.index}
+from models import Sequence
 
 
-print(ret)
+async def load_qza(file_path: str) -> list[Sequence]:
+    aft: Artifact = Artifact.load(file_path)
+    sequences = aft.view(qiime2.Metadata)
+    sequences_df = sequences.to_dataframe()
+    ret = {i: sequences_df.loc[i, "Sequence"] for i in sequences_df.index}
+    return [Sequence(id=i, sequence=ret[i]) for i in ret]
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    res = loop.run_until_complete(load_qza("rep-seqs.qza"))
+    print(res)
